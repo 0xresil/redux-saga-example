5731dad2c997e8114124b7a59e4ce3d0aa42fe2c
blockchainlover2019 <blockchainlover2019@gmail.com>
2023-03-11 04:04:48 +0900

    feat: build directory structure
    This arranges directory structures and addes tailwind css.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <span> This is HomePage. </span>
+  </div>
+)
+
+export default Home;
